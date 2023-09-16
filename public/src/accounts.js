1525ec510a965f3c065b8cb22bba256a0fc2bb62
Cenriquez23 <cenriquez596@gmail.com>
2023-09-15 23:06:19 -0400

    Complete qualified assignment, ...

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,26 +1,27 @@
-ll.sp'/89const findAccountById = (accounts, id) =>
-// YOUR SOLUTION HERE
-// Hint: You can use the [`find()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find) method here. 
+const findAccountById = (accounts, id) =>
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`find()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find) method here. 
 accounts.find((account) => account.id === id);
 
 const sortAccountsByLastName = (accounts) => {
-// YOUR SOLUTION HERE
-// Hint: You can use the [`sort()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) method here. 
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`sort()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) method here. 
 return accounts.sort((a, b) =>
-  a.name.last.toLowerCase().localeCompare(b.name.last.toLowerCase()
-)
-  );
-};
+    a.name.last.toLowerCase().localeCompare(b.name.last.toLowerCase()
+  )
+    );
+  };
 
 function getAccountFullNames(accounts) {
-// YOUR SOLUTION HERE
-// Hint: You can use the [`map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map) method here.
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map) method here.
 let result = [];
-for (let i = 0; i < accounts.length; i++) {
+  for (let i = 0; i < accounts.length; i++) {
 result.push(accounts[i].name.first + " " + accounts[i].name.last);
 }
 return result; 
 }; 
+
 
 // NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
 function getTotalNumberOfBorrows(account, books) {
